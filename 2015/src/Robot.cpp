22370cac1ff4ce20d8ad9471adcf1a067f2320b6
SecretAgentNine <secretagentnine@github.com>
2015-01-23 15:49:17 -0600

    + primitive brakes

--- 2015/src/Robot.cpp
+++ 2015/src/Robot.cpp
@@ -9,6 +9,10 @@ class Robot: public IterativeRobot {
 	Talon *spindle 		= new Talon(4);
 	Talon *grip 		= new Talon(5);
 
+	float brakeRotate = -0.2;
+
+
+
 	CameraServer *server = CameraServer::GetInstance();
 	float camrot = 0.75f;
 private:
@@ -38,8 +42,16 @@ private:
 		robot->MecanumDrive_Cartesian(rstick->GetX(), rstick->GetY(), (float) rstick->GetRawButton(4)-(float) rstick->GetRawButton(5));
 		spindle->Set(lstick->GetY());
 		grip->Set(((float) lstick->GetRawButton(3))-((float) lstick->GetRawButton(2)));
-	}
+		if(!(rstick->GetButton(1))){
 
+
+
+		}else{
+			spindle->SetSpeed(brakeRotate);
+
+		}
+
+	}
 	void TestPeriodic() {
 		lw->Run();
 	}
