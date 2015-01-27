cba6bbd26b8b4eda579deafe5f15adcb262681d2
SecretAgentNine <secretagentnine@github.com>
2015-01-27 17:32:11 -0600

    Fixed inheritance errors, annotated code

--- 2015/src/Robot.cpp
+++ 2015/src/Robot.cpp
@@ -1,21 +1,33 @@
 #include "WPILib.h"
+#include "math.h"
 
 class Robot: public IterativeRobot {
+	//Joysticks
 	Joystick *rstick 	= new Joystick(1);
 	Joystick *lstick 	= new Joystick(0);
 
+	//Motor controllers
 	RobotDrive *robot 	= new RobotDrive(0, 1, 2, 3);
-	Servo *cam_motor 	= new Servo(6);
 	Talon *spindle 		= new Talon(4);
 	Talon *grip 		= new Talon(5);
+	Servo *cam_motor 	= new Servo(6);
+	Servo *ultra_motor  = new Servo(7);
 
+	//Ultrasonic object & variables
+	AnalogInput *ultra = new AnalogInput(3);
+	float vpi = 5.0/512;
+	float distance = 0.0f;
+
+	//Camera output & variables
 	CameraServer *server = CameraServer::GetInstance();
-	float camrot = 0.75f;
+	float camrot = 0.5f;
 private:
 	LiveWindow *lw;
 
 	void RobotInit() {
 		lw = LiveWindow::GetInstance();
+
+		//Begin camera stream
 		server->SetQuality(50);
 		server->StartAutomaticCapture("cam1");
 	}
@@ -33,12 +45,22 @@ private:
 	}
 
 	void TeleopPeriodic() {
+		//Control camera servo with 2 and 3 buttons on RSTICK
 		camrot = camrot + ((float) rstick->GetRawButton(3))*0.01 - ((float) rstick->GetRawButton(2))*0.01;
 		cam_motor->Set(camrot);
-		robot->MecanumDrive_Cartesian((float) rstick->GetRawButton(4)-(float) rstick->GetRawButton(5),
-				rstick->GetX(), rstick->GetY());
+
+		//Drive the robot with mecanum drive
+		//RSTICK X is strafe, RSTICK Y is f/b, RSTICK buttons 4 & 5 are rotation
+		robot->MecanumDrive_Cartesian(pow(rstick->GetX(),3),  	//curved for precision control
+				(float) rstick->GetRawButton(4)-(float) rstick->GetRawButton(5),
+				-rstick->GetY());
+
+		//Control the forklift's grip actuator
 		grip->Set(((float) lstick->GetRawButton(3))-((float) lstick->GetRawButton(2)));
-		if(rstick->GetTrigger()){
+
+		//Control the forklift's vertical motion (LTRIGGER + LSTICK Y)
+		//todo: electronic braking
+		if(lstick->GetTrigger()){
 
 			spindle->Set(-lstick->GetY());
 		}
