2aacabb3443160817e6d37278a2464373fac9a58
SecretAgentNine <secretagentnine@github.com>
2015-02-04 16:41:53 -0600

    Cleaned up unused code

--- 2015/src/Robot.cpp
+++ 2015/src/Robot.cpp
@@ -1,14 +1,10 @@
 #include "WPILib.h"
 #include "math.h"
-#include "time.h"
-#include <ctime>
 
 class Robot: public IterativeRobot {
 	//Joysticks
 	Joystick *rstick 	= new Joystick(1);
 	Joystick *lstick 	= new Joystick(0);
-	int up = 1;
-	int down = 0;
 
 	//Motor controllers
 	RobotDrive *robot 	= new RobotDrive(0, 1, 2, 3);
@@ -16,17 +12,14 @@ class Robot: public IterativeRobot {
 	Talon *spindleB		= new Talon(5);
 	Talon *grip 		= new Talon(6);
 	Servo *cam_motor 	= new Servo(7);
-	Servo *ultra_motor 	= new Servo(8);
 
 	//Ultrasonic object & variables
 	AnalogInput *ultra= new AnalogInput(3);
 	float vpi = 5.0 / 512;
-	float distance[10];
-	float reverseVoltage = 0.0;
 
 	//Camera output & variables
 	CameraServer *server = CameraServer::GetInstance();
-	float camrot = 0.25f;
+	float camrot = 0.1f;
 	//enviroment/autonomous variables.
 	int position;
 
@@ -36,43 +29,34 @@ private:
 	void RobotInit() {
 		lw = LiveWindow::GetInstance();
 
+		//Set camera position
+		cam_motor->Set(camrot);
 		//Begin camera stream
 		server->SetQuality(50);
 		server->StartAutomaticCapture("cam1");
 	}
 
 	void AutonomousInit() {
-		//outside is the side with the tote facing wall
-		// 0 is outside, 1 is middle...
-		 position = 0;
+		//Clamp tote
+		grip->Set(1.0);
+		Wait(.25);
+		grip->Set(0);
+		spindleA->Set(1);
+		spindleB->Set(1);
+		robot->MecanumDrive_Cartesian(1,0,0);
+		Wait(1.50);
+		spindleA->Set(0);
+		spindleB->Set(0);
+		Wait(.50);
+		robot->MecanumDrive_Cartesian(0,0,0);
+		grip->Set(-1.0);
+		Wait(.25);
+		grip->Set(0);
 
 
 	}
 
 	void AutonomousPeriodic() {
-		if(position == 0) {
-			//Clamp tote
-			grip->Set(1.0);
-			Wait(.25);
-			grip->Set(0);
-			spindleA->Set(up);
-			spindleB->Set(up);
-			robot->MecanumDrive_Cartesian(1,0,0);
-			Wait(1.50);
-			spindleA->Set(0);
-			spindleB->Set(0);
-			Wait(.50);
-			robot->MecanumDrive_Cartesian(0,0,0);
-			grip->Set(-1.0);
-			Wait(.25);
-			grip->Set(0);
-
-
-		}else if(position == 1){
-
-		}else if(position == 2){
-
-		}
 
 	}
 
@@ -84,25 +68,12 @@ private:
 		//Control camera servo with 2 and 3 buttons on RSTICK
 		camrot = camrot + ((float) rstick->GetRawButton(3)) * 0.01
 				- ((float) rstick->GetRawButton(2)) * 0.01;
-		cam_motor->Set(camrot); //*/
-		//Read and transmit ultrasonic data
-		/*distance[1] = distance[0];
-		distance[0] = ultra->GetAverageVoltage() / vpi;
-		SmartDashboard::PutNumber("Distance:", distance[0]);
-		//if the lift is getting closer....
-		if ((distance[0] - distance[1]) < 0 && !lstick->GetTrigger()) {
-
-			reverseVoltage >= 0.5 ? reverseVoltage = 0.5 : reverseVoltage += .01;
-		} else //if lift is getting further away...
-		if ((distance[0] - distance[1] > 0 && !lstick->GetTrigger())) {
-
-			reverseVoltage <= 0 ? reverseVoltage = 0 : reverseVoltage -= .01;
-		}//*/
+		cam_motor->Set(camrot);
 
 		//Drive the robot with mecanum drive
-		//RSTICK X is strafe, RSTICK Y is f/b, RSTICK buttons 4 & 5 are rotation
+		//RSTICK X is strafe, RSTICK buttons 4 & 5 are rotation, RSTICK Y is f/b
 		robot->MecanumDrive_Cartesian(pow(rstick->GetX(), 3), //curved for precision control
-				-pow(lstick->GetX(),3), -rstick->GetY());
+				-pow(lstick->GetX(),3), -pow(rstick->GetY(),3));
 
 		//Control the forklift's grip actuator TEMPORARY
 		grip->Set(
@@ -112,9 +83,8 @@ private:
 		//Control the forklift's vertical motion (LTRIGGER + LSTICK Y)
 		//todo: electronic braking
 		if (lstick->GetTrigger()) {
-
-			spindleA->Set(-lstick->GetY());
-			spindleB->Set(-lstick->GetY());
+			spindleA->Set(pow(-lstick->GetY(),3));
+			spindleB->Set(pow(-lstick->GetY(),3));
 		} else {
 			spindleA->Set(0);
 			spindleB->Set(0);
